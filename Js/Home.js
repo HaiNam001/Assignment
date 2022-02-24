4e15e35627ca2a035d54cd043b578167fc3116c5
Your Name <you@example.com>
2022-02-24 20:00:06 +0700

    add some image on slider

---
+++ Js/Home.js
@@ -0,0 +1,13 @@
+var index = 1;
+function changeImage(){
+    
+    var imgs =["assets/image/imgslider1.jpg","assets/image/imgslider2.jpg","assets/image/imgslider3.jpg",
+    "assets/image/imgslider4.jpg","assets/image/imgslider5.jpg"];
+document.getElementById('imgsl').src= imgs[index];
+    index++;
+    if(index == 5){
+        index = 0;
+    }
+    
+}
+setInterval(changeImage,5000);
